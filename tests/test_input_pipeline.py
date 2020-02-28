0e2d8933a4d35e535f34e7fc5560cb9297720e9d
yoyota <smarthorse6@gmail.com>
2020-02-28 11:15:23 +0900

    Refactor test input pipeline

--- tests/test_input_pipeline.py
+++ tests/test_input_pipeline.py
@@ -3,8 +3,6 @@ from cnn.input_pipeline import input_fn
 
 def test_input_pipeline():
     dataset = input_fn("s3://tfrecord/forward-head-posture/2020-02-15", 1)
-    t = dataset.take(1)
-    print(dataset)
-    for image, distance in t.as_numpy_iterator():
+    for image, distance in dataset.take(1).as_numpy_iterator():
         print(image)
-        print(distance[0])
+        print(distance)
