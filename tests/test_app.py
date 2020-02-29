1996da1ed6e75298fa460623c0d753a41df7793b
yoyota <smarthorse6@gmail.com>
2020-02-29 11:26:21 +0900

    Skip tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
+import pytest
 from cnn.app import main
 
 
+@pytest.mark.skip
 def test_main():
     main(batch_size=1, model_dir="s3://test/forward-head-posture/keras_ckpt")
--- tests/test_input_pipeline.py
+++ tests/test_input_pipeline.py
@@ -1,6 +1,8 @@
+import pytest
 from cnn.input_pipeline import input_fn
 
 
+@pytest.mark.skip
 def test_input_pipeline():
     dataset = input_fn("s3://tfrecord/forward-head-posture/2020-02-15", 1)
     for image, distance in dataset.take(1).as_numpy_iterator():
