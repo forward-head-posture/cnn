c7584ea54bc4a8d928a9f8b8a03425ecbeaae51b
yoyota <smarthorse6@gmail.com>
2020-03-01 17:58:17 +0900

    Fix keras cache directory

--- cnn/app.py
+++ cnn/app.py
@@ -50,7 +50,7 @@ def count_images(data_dir, pattern="**/*train*"):
 
 # pylint: disable=too-many-arguments
 def main(
-    batch_size=32,
+    batch_size=200,
     data_dir="s3://tfrecord/forward-head-posture",
     decay_rate=0.7,
     decay_steps=100,
